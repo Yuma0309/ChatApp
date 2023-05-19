09c0b8d40daecc4cc8870b16f2f43674cb70974b
中村祐馬 <yuma.nakamura@arsaga.jp>
2023-05-19 15:00:13 +0900

    プロジェクトを作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import SignUp from './components/SignUp'; // SignUpコンポーネントをimport
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: '2em' }}> {/* ユーザー登録画面のマージン（余白）を設定 */}
+      <SignUp />
     </div>
   );
 }
